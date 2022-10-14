cebb5aa5db4b7aaa2fc83bc7c142b2a801c9e814
antenehz <antenehz@rams.colostate.edu>
2022-10-14 15:14:17 -0400

    bah change

---
+++ hello-api/src/main/java/com/bah/spring/sample/hello/api/HelloResponse.java
@@ -0,0 +1,19 @@
+package com.bah.spring.sample.hello.api;
+
+public class HelloResponse {
+String message;
+
+public HelloResponse(String message) {
+	super();
+	this.message = message;
+}
+
+//public String getMessage() {
+//	return message;
+//}
+//
+//public void setMessage(String message) {
+//	this.message = message;
+//}
+
+}
