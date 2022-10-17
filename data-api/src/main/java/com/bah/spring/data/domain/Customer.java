7a947a4cda08f8c42fb907a8fe8af4e2ef45a545
abarbuzza <alessandro.barbuzza@verizon.net>
2022-10-17 13:44:05 -0400

    Worked objects in domain folder

--- data-api/src/main/java/com/bah/spring/data/domain/Customer.java
+++ data-api/src/main/java/com/bah/spring/data/domain/Customer.java
@@ -4,12 +4,53 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Id;
+
 
 @Entity
 @Table(name="CUSTOMERS")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    long id;
+
+    @Column(name="CUSTOMER_NAME")
+    String username;
+    String email;
+    String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String name) {
+        this.username = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
     //  Workshop:
     //
     //  Make a concrete implementation of a customer domain.  What attributes do you need for
--- data-api/src/main/java/com/bah/spring/data/domain/Event.java
+++ data-api/src/main/java/com/bah/spring/data/domain/Event.java
@@ -4,12 +4,60 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Id;
 
 @Entity
 @Table(name="EVENTS")
 public class Event {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    long id;
+    String code;
+    String title;
+    String description;
+
+    public Event(long id, String code, String title, String description) {
+        super();
+        this.id = id;
+        this.code = code;
+        this.title = title;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
     //  Workshop:
     //
     //  Create an Event class, and implement methods to manage event data.
