ba4d8d53edbd9e22d8f8c7ea25d4e982e6703e1f
abarbuzza <alessandro.barbuzza@verizon.net>
2022-10-17 12:50:22 -0400

    Fox's sample code

--- data-api/src/main/java/com/bah/spring/data/DataApiApplication.java
+++ data-api/src/main/java/com/bah/spring/data/DataApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class dataApiApplication {
+public class DataApiApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(dataApiApplication.class, args);
+		SpringApplication.run(DataApiApplication.class, args);
 	}
 }
